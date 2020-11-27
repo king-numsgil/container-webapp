41e21931fda2ca3ca5da53bebdbde81e1c24fc74
Daniel Grondin <king@numsgil.co>
2020-11-27 00:42:24 -0500

    Removed WebVitals as I won't be using that

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,5 +1,4 @@
 import {ChakraProvider, ColorModeScript} from "@chakra-ui/react";
-import reportWebVitals from "./reportWebVitals";
 import {Router, View} from "react-navi";
 import React, {Suspense} from "react";
 import ReactDOM from "react-dom";
@@ -20,8 +19,3 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
