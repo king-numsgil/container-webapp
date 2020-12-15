7c97c06f9afecbdb8ab736d685a04273d97419be
Daniel Grondin <king@numsgil.co>
2020-12-15 00:40:46 -0500

    Remade authentication (with Foal v2) with new class validation!

--- api/src/app/controllers/api.controller.ts
+++ api/src/app/controllers/api.controller.ts
@@ -1,6 +1,12 @@
-import {Context, Get, HttpResponseOK} from "@foal/core";
+import {Context, controller, Get, HttpResponseOK} from "@foal/core";
+
+import {SecurityController} from "./security.controller";
 
 export class ApiController {
+	subControllers = [
+		controller("/security", SecurityController),
+	];
+
 	@Get("/")
 	index(ctx: Context) {
 		return new HttpResponseOK("Hello world!");
---
+++ api/src/app/controllers/security.controller.ts
@@ -0,0 +1,104 @@
+import {IsEmail, IsString, Matches, MaxLength, MinLength} from "class-validator";
+import {fetchUserWithPermissions} from "@foal/typeorm";
+import {ValidateBody} from "@foal/typestack";
+import {
+	Context,
+	createSession,
+	dependency,
+	hashPassword,
+	HttpResponseOK,
+	HttpResponseUnauthorized,
+	Store,
+	Post,
+	UseSessions,
+	verifyPassword, UserRequired
+} from "@foal/core";
+
+import {User} from "../entities";
+import {Match} from "../utils";
+
+class CredentialsDto {
+	@IsEmail()
+	email!: string;
+
+	@IsString()
+	@MinLength(6)
+	@Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {message: "password too weak"})
+	password!: string;
+}
+
+class RegisterDto extends CredentialsDto {
+	@IsString()
+	@MinLength(6)
+	@Match("password")
+	confirm!: string;
+
+	@IsString()
+	@MinLength(2)
+	@MaxLength(32)
+	firstName!: string;
+
+	@IsString()
+	@MinLength(2)
+	@MaxLength(32)
+	lastName!: string;
+}
+
+@UseSessions({
+	user: fetchUserWithPermissions(User),
+})
+export class SecurityController {
+	@dependency
+	store!: Store;
+
+	@Post("/signup")
+	@ValidateBody(RegisterDto)
+	async signup(ctx: Context<User>) {
+		const body = ctx.request.body as RegisterDto;
+		const user = await User.create({
+			email: body.email,
+			password: await hashPassword(body.password),
+			firstName: body.firstName,
+			lastName: body.lastName,
+		}).save();
+
+		ctx.session = await createSession(this.store);
+		ctx.session.setUser(user);
+
+		return new HttpResponseOK({
+			token: ctx.session.getToken(),
+		});
+	}
+
+	@Post("/login")
+	@ValidateBody(CredentialsDto)
+	async login(ctx: Context<User>) {
+		const body = ctx.request.body as CredentialsDto;
+		const user = await User.findOne({email: body.email});
+
+		if (!user) {
+			return new HttpResponseUnauthorized();
+		}
+
+		if (!await verifyPassword(body.password, user.password)) {
+			return new HttpResponseUnauthorized();
+		}
+
+		ctx.session = await createSession(this.store);
+		ctx.session.setUser(user);
+
+		return new HttpResponseOK({
+			token: ctx.session.getToken(),
+		});
+	}
+
+	@Post("/logout")
+	@UserRequired()
+	async logout(ctx: Context<User>) {
+		if (ctx.session) {
+			await ctx.session.destroy();
+		}
+
+		return new HttpResponseOK();
+	}
+}
