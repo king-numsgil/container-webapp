7d41d7abd40826b3bcb32686befae0e5b04dcdfb
Daniel Grondin <king@numsgil.co>
2021-03-25 14:33:57 -0400

    Added specialized component for NavBar Button

--- web/src/components/Page.tsx
+++ web/src/components/Page.tsx
@@ -1,20 +1,44 @@
+import {Button, ButtonProps, Flex} from "@chakra-ui/react";
 import React, {FunctionComponent} from "react";
-import {Button, Flex} from "@chakra-ui/react";
+import {useNavigation} from "react-navi";
+import {URLDescriptor} from "navi";
 import {NavBar} from "./NavBar";
+
+interface NavButtonProps extends Exclude<ButtonProps, "type" | "aria-label" | "variant" | "color" | "onClick"> {
+	label: string;
+	target: string | Partial<URLDescriptor>;
+}
+
+const NavButton: FunctionComponent<NavButtonProps> = props => {
+	const {
+		label,
+		target,
+		...rest
+	} = props;
+
+	const nav = useNavigation();
+
+	return <Button
+		type="button"
+		aria-label={label}
+		variant="outline"
+		color="current"
+		onClick={async event => {
+			event.preventDefault();
+			await nav.navigate(target);
+		}}
+		{...rest}
+	>
+		{label}
+	</Button>;
+};
 
 export const Page: FunctionComponent = props => {
 	const {children} = props;
 
 	return <>
 		<NavBar brand="Numsgil Co">
-			<Button
-				type="button"
-				aria-label="Home"
-				variant="outline"
-				color="current"
-			>
-				Home
-			</Button>
+			<NavButton label="Home" target="/" />
 		</NavBar>
 
 		<Flex
